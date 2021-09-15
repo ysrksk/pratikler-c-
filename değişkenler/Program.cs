613ecc5cd40238cd41ce1fea2b1b56a66ba86f95
Yaşar Küskü <yasarkusku@hotmail.com>
2021-09-15 22:41:55 +0300

    değişkenler

--- değişkenler/Program.cs
+++ değişkenler/Program.cs
@@ -6,7 +6,42 @@ namespace değişkenler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+
+            byte b = 5;   //1byte
+            sbyte c = 5;  //1byte
+
+            short s = 5; //2byte
+            ushort us = 5; //2byte
+
+            int16 i16 = 5; //2byte
+            int i = 5; //4byte
+            int32 i32 = 5; //4byte
+            int64 i64 = 5; //8byte
+
+            uint ui = 2; //4bye
+            long l = 2; //8 byte
+            ulong ul = 2; //8byte
+
+            //reel sayılar
+
+            float f = 2; //4byte
+            double d = 2; //8byte
+            decimal de = 2; //16byte
+
+            char c = '2'; //2byte
+            string str = 'zikriye';  //sınırsız
+
+            bool b2 = true;
+            bool b3 = false;
+
+            DateTime dt = Datetime.Now
+
+
+
+
+
+
         }
     }
 }
