8637dcedd326d7417ea87b9c29ba3cc86db2acc9
Yaşar Küskü <yasarkusku@hotmail.com>
2021-09-15 22:30:09 +0300

    first

---
+++ değişkenler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace değişkenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
